0f2bcb23cf96438728b522c6f086ad19c0e80e88
Gourav Tiwari <gouravtiwari21@gmail.com>
2020-03-29 18:07:31 -0700

    Add Disclaimer page and transition to dummy (tab1 page) on accept

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./disclaimer/disclaimer.module').then( m => m.DisclaimerPageModule)
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'disclaimer',
-    loadChildren: () => import('./disclaimer/disclaimer.module').then( m => m.DisclaimerPageModule)
+    path: 'tabs/tab1',
+    loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
   }
 ];
 @NgModule({
--- src/app/disclaimer/disclaimer.page.ts
+++ src/app/disclaimer/disclaimer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-disclaimer',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisclaimerPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  gotoTab1() {
+    console.log("I am on tab1");
+    this.navCtrl.navigateRoot('/tabs')
   }
 
 }
