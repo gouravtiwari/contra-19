0ea93d0a93b1a438a6a25bbaa27424b034f6ecce
Gourav Tiwari <gouravtiwari21@gmail.com>
2020-04-22 09:44:42 -0700

    Add symptom model

---
+++ src/app/models/symptom.ts
@@ -0,0 +1,11 @@
+export class Symptom {
+  date_ts: number;
+  created_ts: number;
+  updated_ts: number;
+  fever: number;
+  weakness: boolean;
+  dry_cough: boolean;
+  sore_throat: boolean;
+  runny_nose: boolean;
+  difficulty_in_breathing: boolean;
+}
