6279f7bb06b98b5f7ab1bf59bdd0bb2fcf124942
Gourav Tiwari <gouravtiwari21@gmail.com>
2020-04-22 09:15:17 -0700

    QR code is default after accepting terms and conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { environment } from '../environments/environment';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +16,19 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthenticationService,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      if (!environment.showTAndC && this.authService.currentUserValue) {
+        this.router.navigateByUrl('/qr-code');
+      }
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
