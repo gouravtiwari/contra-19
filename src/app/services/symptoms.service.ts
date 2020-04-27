b0f6cec7992cd320ff09390d0b50cae45f17bebb
Gourav Tiwari <gouravtiwari21@gmail.com>
2020-04-26 22:32:08 -0700

    Add symptoms to localStorage

--- src/app/services/symptoms.service.ts
+++ src/app/services/symptoms.service.ts
@@ -8,34 +8,49 @@ import { environment } from '../../environments/environment';
 })
 export class SymptomsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.initSymptomsStorage();
+  }
 
   symptomsShow(idToken, observed_date): Observable<any> {
     return this.http.get(`${environment.apiUrl}/symptoms/${observed_date}?idToken=${idToken}`);
   }
 
-  symptomsUpdate(idToken, observed_date): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        "Accept": 'application/json',
-        'Content-Type': 'application/json'
-      })
-    };
-
-    let requestData = {
-      'idToken': idToken,
-      'symptom': {
-        'observed_date': '',
-        'fever': '98.4',
-        'weakness': true,
-        'dry_cough': false,
-        'sore_throat': true,
-        'runny_nose': false,
-        'difficulty_in_breathing': true
-      }
+  initSymptomsStorage() {
+    const isSymptomRecords = localStorage.getItem('symptomRecords');
+    if (!isSymptomRecords) {
+      localStorage.setItem('symptomRecords', '{}');
     }
+  }
 
-    return this.http.patch(`${environment.apiUrl}/symptoms/${observed_date}`, requestData, httpOptions);
+  getSymptomRecords() {
+    return JSON.parse(localStorage.getItem('symptomRecords'));
+  }
+
+  addOrUpdateSymptom(date, symptoms) {
+    const symptomRecords = this.getSymptomRecords()
+    symptomRecords[date] = symptoms;
+
+    localStorage.setItem('symptomRecords', JSON.stringify(symptomRecords));
+  }
+
+  update(symptomsPayload): Object {
+    console.log(symptomsPayload);
+    let observedDate = symptomsPayload.observed_date === '' ? new Date().toDateString() : symptomsPayload.observed_date;
+    observedDate = new Date(observedDate).toDateString();
+
+    const symptoms = {
+      fever: symptomsPayload.fever,
+      weakness: symptomsPayload.weakness,
+      dry_cough: symptomsPayload.dry_cough,
+      sore_throat: symptomsPayload.sore_throat,
+      runny_nose: symptomsPayload.runny_nose,
+      difficulty_in_breathing: symptomsPayload.difficulty_in_breathing
+    }
+    this.addOrUpdateSymptom(observedDate, symptoms);
+    return {
+      success: "Logged Symptoms successfully"
+    };
   }
 
   symptomsCreate(idToken): Observable<any> {
--- src/app/symptoms/symptoms.page.ts
+++ src/app/symptoms/symptoms.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, Platform } from '@ionic/angular';
+import {  Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from  "@angular/router";
+import { SymptomsService } from '../services/symptoms.service';
 
 @Component({
   selector: 'app-symptoms',
@@ -8,24 +10,46 @@ import { Router } from  "@angular/router";
   styleUrls: ['./symptoms.page.scss'],
 })
 export class SymptomsPage implements OnInit {
+  private symptomsFormGroup : FormGroup;
+
   today: string;
   minDate: string;
   maxDate: string;
 
   constructor(private router: Router,
     private platform: Platform,
-    private navCtrl: NavController
-    ) { }
+    private navCtrl: NavController,
+    private formBuilder: FormBuilder,
+    private symptomsService: SymptomsService
+    ) {
+      this.symptomsFormGroup = this.formBuilder.group({
+        observed_date: [''],
+        fever: ['', Validators.required],
+        weakness: [false],
+        dry_cough: [false],
+        sore_throat: [false],
+        runny_nose: [false],
+        difficulty_in_breathing: [false]
+      });
+    }
 
   ngOnInit() {
-    let today = new Date();
-    let twoWeeksAgo = new Date();
+    const today = new Date();
+    const twoWeeksAgo = new Date();
     twoWeeksAgo.setDate(today.getDate() - 14);
 
-    this.today = today.toISOString();
+    this.today = new Date(today.toDateString()).toISOString();
     this.maxDate = this.today;
-    this.minDate = twoWeeksAgo.toISOString();
+    this.minDate = new Date(twoWeeksAgo.toDateString()).toISOString();
 
+  }
+
+  symptoms() {
+    console.log("Symptoms");
+    console.log();
+
+    const res = this.symptomsService.update(this.symptomsFormGroup.value);
+    console.log(res);
   }
 
   userProfile() {
